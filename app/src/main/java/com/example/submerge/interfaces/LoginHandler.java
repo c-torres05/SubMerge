d07c6d564e94580e84f734495ef3c34a239ed0cc
TheEman1can <ethan.wolfe212@gmail.com>
2020-03-09 23:35:57 -0700

    update main view with new ids and update write login login handler to interface with database handler

--- app/src/main/java/com/example/submerge/interfaces/LoginHandler.java
+++ app/src/main/java/com/example/submerge/interfaces/LoginHandler.java
@@ -25,7 +25,7 @@ public class LoginHandler extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.logon);
+        setContentView(R.layout.login);
 
         this.anonymousLogin = findViewById(R.id.anon_login_button);
         this.anonymousLogin.setOnClickListener(this::anonLogin);
