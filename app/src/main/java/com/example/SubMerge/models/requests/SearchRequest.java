7f3ddddf67fb8be2df32e0c1013c821cb8fa68b0
sarecasm <sarahhassan98@gmail.com>
2020-03-14 19:22:02 -0700

    SearchRequest Upload ??

---
+++ app/src/main/java/com/example/SubMerge/models/requests/SearchRequest.java
@@ -0,0 +1,11 @@
+package com.example.SubMerge.models.requests;
+
+import com.example.submerge.models.Subscription;
+import com.example.submerge.models.User;
+
+public class SearchRequest extends com.example.submerge.models.requests.Request {
+    public SearchRequest(User user, Subscription subscription) {
+        super(user, subscription);
+        this.type = com.example.submerge.models.requests.Request.SUBSCRIPTION_TYPE;
+    }
+}
