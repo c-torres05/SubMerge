137994f7f8b2a9e0da34a488f41a03108a37edd2
TheEman1can <ethan.wolfe212@gmail.com>
2020-03-15 16:38:12 -0700

    Fix importing

--- app/src/main/java/com/example/SubMerge/models/requests/SearchRequest.java
+++ app/src/main/java/com/example/SubMerge/models/requests/SearchRequest.java
@@ -3,9 +3,9 @@ package com.example.submerge.models.requests;
 import com.example.submerge.models.Subscription;
 import com.example.submerge.models.User;
 
-public class SearchRequest extends com.example.submerge.models.requests.Request {
+public class SearchRequest extends Request {
     public SearchRequest(User user, Subscription subscription) {
         super(user, subscription);
-        this.type = com.example.submerge.models.requests.Request.SUBSCRIPTION_TYPE;
+        this.type = SUBSCRIPTION_TYPE;
     }
 }
