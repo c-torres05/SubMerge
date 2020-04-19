41ae279cd5bde3d2e6f55c67573afc1a4fbb9681
TheEman1can <ethan.wolfe212@gmail.com>
2020-04-18 18:46:26 -0700

    Add an item click listener class for the recyclerview to better handle getting the individual object's parameters.

---
+++ app/src/main/java/com/example/submerge/interfaces/RecyclerItemClickListener.java
@@ -0,0 +1,113 @@
+package com.example.submerge.interfaces;
+
+import android.view.View;
+import androidx.recyclerview.widget.RecyclerView;
+import com.example.submerge.R;
+
+/*
+OnClickItem support for the recyclerview
+Usage:
+RecyclerItemClickListener.addTo(recyclerview_object).setOnItemClickListener((recyclerView, position, v) -> method_to_handle_click(position));
+//method_to_handle_click will then use the posistion to get the object from the list and use that data to make the next screen
+ */
+
+
+public class RecyclerItemClickListener {
+    private final RecyclerView mRecyclerView;
+    private OnItemClickListener mOnItemClickListener;
+    private OnItemLongClickListener mOnItemLongClickListener;
+
+    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (mOnItemClickListener != null) {
+                // ask the RecyclerView for the viewHolder of this view.
+                // then use it to get the position for the adapter
+                RecyclerView.ViewHolder holder = mRecyclerView.getChildViewHolder(v);
+                mOnItemClickListener.onItemClicked(mRecyclerView, holder.getAdapterPosition(), v);
+            }
+        }
+    };
+
+    private View.OnLongClickListener mOnLongClickListener = new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View v) {
+            if (mOnItemLongClickListener != null) {
+                RecyclerView.ViewHolder holder = mRecyclerView.getChildViewHolder(v);
+                return mOnItemLongClickListener.onItemLongClicked(mRecyclerView, holder.getAdapterPosition(), v);
+            }
+            return false;
+        }
+    };
+
+    private RecyclerView.OnChildAttachStateChangeListener mAttachListener
+            = new RecyclerView.OnChildAttachStateChangeListener() {
+        @Override
+        public void onChildViewAttachedToWindow(View view) {
+            // every time a new child view is attached add click listeners to it
+            if (mOnItemClickListener != null) {
+                view.setOnClickListener(mOnClickListener);
+            }
+            if (mOnItemLongClickListener != null) {
+                view.setOnLongClickListener(mOnLongClickListener);
+            }
+        }
+
+        @Override
+        public void onChildViewDetachedFromWindow(View view) {
+
+        }
+    };
+
+    private RecyclerItemClickListener(RecyclerView recyclerView) {
+        mRecyclerView = recyclerView;
+        // the ID must be declared in XML, used to avoid
+        // replacing the ItemClickSupport without removing
+        // the old one from the RecyclerView
+        mRecyclerView.setTag(R.id.item_click_support, this);
+        mRecyclerView.addOnChildAttachStateChangeListener(mAttachListener);
+    }
+
+    public static RecyclerItemClickListener addTo(RecyclerView view) {
+        // if there's already an ItemClickSupport attached
+        // to this RecyclerView do not replace it, use it
+        RecyclerItemClickListener support = (RecyclerItemClickListener) view.getTag(R.id.item_click_support);
+        if (support == null) {
+            support = new RecyclerItemClickListener(view);
+        }
+        return support;
+    }
+
+    public static RecyclerItemClickListener removeFrom(RecyclerView view) {
+        RecyclerItemClickListener support = (RecyclerItemClickListener) view.getTag(R.id.item_click_support);
+        if (support != null) {
+            support.detach(view);
+        }
+        return support;
+    }
+
+    public RecyclerItemClickListener setOnItemClickListener(OnItemClickListener listener) {
+        mOnItemClickListener = listener;
+        return this;
+    }
+
+    public RecyclerItemClickListener setOnItemLongClickListener(OnItemLongClickListener listener) {
+        mOnItemLongClickListener = listener;
+        return this;
+    }
+
+    private void detach(RecyclerView view) {
+        view.removeOnChildAttachStateChangeListener(mAttachListener);
+        view.setTag(R.id.item_click_support, null);
+    }
+
+    public interface OnItemClickListener {
+
+        void onItemClicked(RecyclerView recyclerView, int position, View v);
+    }
+
+    public interface OnItemLongClickListener {
+
+        boolean onItemLongClicked(RecyclerView recyclerView, int position, View v);
+    }
+}
